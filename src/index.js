d0c76936b17bd9b920e9cd41cab55137a1f1ebe6
robbiemurray98 <robbiemurray98@yahoo.com>
2026-02-18 19:47:51 -0500

    create form functionality

--- src/index.js
+++ src/index.js
@@ -1,19 +1,14 @@
 import "./styles.css";
 import { mobileMenu } from "./mobile-menu.js";
-import { loadHeader, loadDesktopHeader } from "./header.js";
 import { loadMobileSidebar } from "./mobileSidebar.js";
-import { loadFooter } from "./footer.js";
-// import { loadHowManyDropdown } from "./how-many-dropdown.js"
 import { checkPurchaseMethod } from "./purchase-form.js";
 
-// loadHeader();
-// loadDesktopHeader();
+
 loadMobileSidebar();
 mobileMenu();
-// loadFooter();
 
 
-// 
+
 const changeHeaderBorder = () => {
     const header = document.querySelector('#header');
     const desktopHeader = document.querySelector('#desktop-header')
@@ -109,26 +104,24 @@ addFooterBorder();
 // }
 // addHowManyDropdown();
 
-// const purchaseSubmit = () => {
-//     const purchaseSubmitBtn = document.querySelector('#purchase-submit-btn');
-//     const currentPath = window.location.pathname;
+const purchaseSubmit = () => {
+    const purchasePageForm = document.querySelector('#purchase-page-form');
+    const currentPath = window.location.pathname;
 
-//     if(currentPath.includes('where-to-buy.html')){
+    if(currentPath.includes('where-to-buy.html')){
 
-//         purchaseSubmitBtn.addEventListener('click', (event) => {
-//         const pickupDelivery = document.querySelector('#pickup-delivery');
-//         const selectValue = pickupDelivery.value;
+        purchasePageForm.addEventListener('submit', () => {
+        const pickupDelivery = document.querySelector('#pickup-delivery');
+        const selectValue = pickupDelivery.value;
 
-//         event.preventDefault();
 
-//         sessionStorage.setItem('orderType', selectValue);
+        sessionStorage.setItem('orderType', selectValue);
 
-//         window.location.href = 'thank-you-page.html';
-//     })
-//     }
+    })
+    }
 
-// }
-// purchaseSubmit();
+}
+purchaseSubmit();
 
 const confirmOrderType = () => {
     const delivery = document.querySelector('#delivery');
@@ -169,6 +162,170 @@ const purchasePageForm = () => {
 }
 
 purchasePageForm();
+
+
+
+
+const handleLandingPageForm = () => {
+    const currentPath = window.location.pathname;
+    const landingForm = document.querySelector('#landing-contact-form');
+
+    if(currentPath.includes('index.html')){
+        landingForm.addEventListener('submit', async function(event){
+            event.preventDefault();
+
+            const formData = new FormData(event.target);
+            const data = Object.fromEntries(formData.entries());
+
+            try {
+                const response = await fetch('https://api.web3forms.com/submit', {
+                    method: 'POST',
+                    headers: {
+                        'content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                });
+
+                if(response.ok){
+                    landingForm.reset();
+                    console.log('Form submitted successfully');
+                } else{
+                    const result = await response.json();
+                    console.error('Form submission failed', result.message, result)
+                }
+            } catch(error){
+                console.error('Error during form submission:', error);
+            }
+        });
+
+
+        const modal = document.querySelector('#landing-modal')
+        const closeModal = document.querySelector('.close-button');
+
+
+        const formElements = landingForm.querySelectorAll('input, textarea');
+
+        function isFormEmpty(){
+            for(let i = 0; i < formElements.length; i++){
+            const element = formElements[i];
+
+            if(element.type === 'submit' || element.type === 'hidden' || element.className === 'not-required')  {
+                continue
+            } 
+
+            if(element.value.trim() === ''){
+                return true;
+            }
+        }
+
+        modal.showModal();
+
+        }
+
+    landingForm.addEventListener('submit', () => {
+
+        isFormEmpty();
+
+    closeModal.addEventListener('click', () => {
+        modal.close();
+    })
+    })
+    }
+}
+
+
+handleLandingPageForm();
+
+
+
+
+
+
+const handleContactPageForm = () => {
+    const currentPath = window.location.pathname;
+
+    if(currentPath.includes('contact.html')){
+        document.querySelector('#contact-page-form').addEventListener('submit', async function(event){
+        event.preventDefault();
+
+        const formData = new FormData(event.target);
+        const data = Object.fromEntries(formData.entries());
+
+        try {
+            const response = await fetch('https://api.web3forms.com/submit', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            if(response.ok) {
+                contactForm.reset();
+                console.log('Form submitted successfully!');
+            } else {
+                const result = await response.json();
+                console.error('Form submission failed:', result.message, result)
+            }
+        } catch(error) {
+            console.error('Error during form submission:', error);
+        }
+    });
+
+
+        const contactForm = document.querySelector('#contact-page-form');
+
+        const modal = document.querySelector('#modal')
+        const closeModal = document.querySelector('.close-button');
+
+
+        const formElements = contactForm.querySelectorAll('input, textarea');
+
+        function isFormEmpty(){
+            for(let i = 0; i < formElements.length; i++){
+            const element = formElements[i];
+
+            if(element.type === 'submit' || element.type === 'hidden' || element.className === 'not-required')  {
+                continue
+            } 
+
+            if(element.value.trim() === ''){
+                return true;
+            }
+        }
+
+        modal.showModal();
+
+        // contactForm.addEventListener('submit', function(event){
+        //     contactForm.reset();
+        // })
+    }
+
+    contactForm.addEventListener('submit', () => {
+
+        isFormEmpty();
+
+    closeModal.addEventListener('click', () => {
+        modal.close();
+    })
+    })
+
+
+    }
+}
+
+handleContactPageForm();
+
+
+
+
+
+
+
+
+
+
+// handleModal();
 
 
 
