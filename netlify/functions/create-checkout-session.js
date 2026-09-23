643b54e7310a3b1b8cf8fb5bac09978fc6baaabf
robbiemurray98 <robbiemurray98@yahoo.com>
2026-09-22 21:18:12 -0400

    update return url

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -51,7 +51,7 @@ exports.handler = async (event) => {
       ],
       mode: 'payment',
       // return_url: 'https://playaceball.com/thank-you-page.html?session_id={CHECKOUT_SESSION_ID}',
-      return_url: 'https://aceball.com/gthank-you-page',
+      return_url: 'https://playaceball.com/gthank-you-page',
     });
 
     // Return the client secret to the frontend
